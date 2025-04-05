6298455d785bb0029edfefc5da488043a1d64048
Edvin Lundström <edvin.lundstrom@gmail.com>
2025-04-05 18:37:32 +0200

    Avoid zero division error

--- main.py
+++ main.py
@@ -60,9 +60,10 @@ def main():
     a = st.slider(
         "Amount of information", min_value=0.0, max_value=0.995, value=0.8, step=0.05
     )
-    st.write(
-        f"Time to {a:.1%} of info: {math.log(1 - a) / math.log(decay_A):.1f} days for A, {math.log(1 - a) / math.log(decay_B):.1f} days for B"
-    )
+    if decay_A < 1 and decay_B < 1:
+        st.write(
+            f"Time to {a:.1%} of info: {math.log(1 - a) / math.log(decay_A):.1f} days for A, {math.log(1 - a) / math.log(decay_B):.1f} days for B"
+        )
 
     data = {
         "A": get_weights(int(DAYS_IN_YEAR * n_months_A / 12), decay_A, normalize),
